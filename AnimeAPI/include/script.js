2ae6196f46a9e5841ca5a0f9fccf1f5d617036ef
Kiran G <kirangpay@gmail.com>
2021-10-21 14:47:46 +0530

    size fix

--- AnimeAPI/include/script.js
+++ AnimeAPI/include/script.js
@@ -29,8 +29,9 @@ async function getAnime() {
 <div
       class="
   ]     col-12 col-md-10 col-lg-8 col-xl-6
-        m-2
-        p-3
+        m-1
+        mb-2
+        pb-3
         rounded
         justify-content-center
         align-items-center
