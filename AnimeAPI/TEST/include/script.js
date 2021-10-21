8d5743af3fba56d9ca47e5cf94452f00c12ecc3f
Kiran G <kirangpay@gmail.com>
2021-10-21 12:53:52 +0530

    submit

---
+++ AnimeAPI/TEST/include/script.js
@@ -0,0 +1,40 @@
+document.body.innerHTML = `<div class="col-md-6 pg-title align-content-center">Anime Page- Naruto
+</div>
+<section class="anime-page"></section>`;
+
+async function getAnime() {
+  const data = await fetch("https://api.jikan.moe/v3/search/anime?q=naruto", {
+    method: "GET",
+  });
+  const allAnimes = await data.json();
+  const animes = allAnimes.results;
+
+  const animeContainer = document.querySelector(".anime-page");
+  animeContainer.innerHTML = "";
+
+  animes.forEach((anime) => {
+    var dateS = new Date(anime.start_date);
+    var dateStart = dateS.toLocaleDateString();
+    var dateE = new Date(anime.end_date);
+    var dateEnd = dateE.toLocaleDateString();
+
+    animeContainer.innerHTML += `
+    <div class="anime-container">
+    <img src="${anime.image_url}"/>
+    <div>
+    <p>Title: <span>${anime.title}</span></p>
+    <p>Type: <span>${anime.type}</span></p>
+    <p>Start Date: <span>${dateStart}</span></p>
+    <p>End Date: <span>${dateEnd}</span></p>
+    <p>Epipsodes: <span>${anime.episodes}</span></p>
+    <p>IMDB Rating: <span>${anime.score}<IMDB RAting:/span></p>
+    <a href="${anime.url}" target="_blank" type="button" class="btn btn-primary">Details</a>
+    </div>
+    </div>
+  `;
+  });
+
+  console.log(animes);
+}
+
+getAnime();
--- AnimeAPI/include/script.js
+++ AnimeAPI/include/script.js
@@ -1,6 +1,12 @@
-document.body.innerHTML = `<div class="col-md-6 pg-title align-content-center">Anime Page- Naruto
+document.body.innerHTML = `
+<div class="row d-flex justify-content-center">
+<div class="col-8 col-md-6 col-lg-5 col-xl-4 col-xxl-4 pg-title text-center fw-bolder mt-4 mb-2"><i class="fad fa-tv-retro"></i> Anime Page- Naruto
 </div>
-<section class="anime-page"></section>`;
+</div>
+<section class="container">
+<div class="row anime-page align-items-center justify-content-center gx-5">
+
+</section>`;
 
 async function getAnime() {
   const data = await fetch("https://api.jikan.moe/v3/search/anime?q=naruto", {
@@ -10,7 +16,7 @@ async function getAnime() {
   const animes = allAnimes.results;
 
   const animeContainer = document.querySelector(".anime-page");
-  animeContainer.innerHTML = "";
+  animeContainer.innerHTML = ``;
 
   animes.forEach((anime) => {
     var dateS = new Date(anime.start_date);
@@ -19,22 +25,79 @@ async function getAnime() {
     var dateEnd = dateE.toLocaleDateString();
 
     animeContainer.innerHTML += `
-    <div class="anime-container">
-    <img src="${anime.image_url}"/>
-    <div>
-    <p>Title: <span>${anime.title}</span></p>
-    <p>Type: <span>${anime.type}</span></p>
-    <p>Start Date: <span>${dateStart}</span></p>
-    <p>End Date: <span>${dateEnd}</span></p>
-    <p>Epipsodes: <span>${anime.episodes}</span></p>
-    <p>IMDB Rating: <span>${anime.score}<IMDB RAting:/span></p>
-    <a href="${anime.url}" target="_blank" type="button" class="btn btn-primary">Details</a>
-    </div>
+
+<div
+      class="
+        col-12 col-md-8 col-lg-6
+        m-2
+        p-3
+        bg-light
+        rounded
+        justify-content-center
+        align-items-center
+        d-md-flex
+        flex-column
+      "
+    >
+      <div class="row justify-content-center align-items-center d-flex">
+        <div class="col-sm-12 fw-bold fs-3 d-flex justify-content-center align-items-center">
+          <p>
+            <i class="fad fa-play"></i> Title:
+            <span class="animeTitle">${anime.title}</span>
+          </p>
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center d-flex">
+        <div class="col-12 col-md-6 justify-content-center d-flex">
+          <img src="${anime.image_url}" />
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="row justify-content-center d-flex">
+            <p>
+              <i class="fas fa-camera-movie"></i>
+              <span class="sub-title">Type: </span><span>${anime.type}</span>
+            </p>
+            <p>
+              <i class="fad fa-calendar-check"></i>
+              <span class="sub-title">Start Date: </span
+              ><span>${dateStart}</span>
+            </p>
+            <p>
+              <i class="fad fa-calendar-times"></i>
+              <span class="sub-title">End Date: </span><span>${dateEnd}</span>
+            </p>
+            <p>
+              <i class="fad fa-film"></i>
+              <span class="sub-title">Episodes: </span
+              ><span>${anime.episodes}</span>
+            </p>
+            <p>
+              <i class="fad fa-registered"></i>
+              <span class="sub-title">Rated: </span><span>${anime.rated}</span>
+            </p>
+            <p>
+              <i class="fas fa-star-half-alt"></i>
+              <span class="sub-title">IMDB Rating: </span
+              ><span>${anime.score}<IMDB Score:</span>
+            </p>
+            <div class="d-grid d-md-block">
+              <a
+                href="${anime.url}"
+                target="_blank"
+                type="button"
+                class="btn btn-primary"
+                ><i class="fad fa-angle-double-right"></i> More Details
+                <i class="fad fa-external-link"></i
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   `;
   });
 
   console.log(animes);
 }
-
+// <div class="row justify-content-center align-items-center m-4 p-3 bg-light d-md-flex rounded"></div>
 getAnime();
